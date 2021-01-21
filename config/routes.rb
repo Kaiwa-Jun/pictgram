f93ea04a3260d9906ccd0d031a8b7afde36d6575
kaiwa <kj.11235813213455@gmail.com>
2021-01-21 13:41:09 +0000

    create_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/new'
+  root 'pages#index'
   get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  resources :users
 end
